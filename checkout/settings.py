84955194ff2e53b331b6b3314e3d5b4fdb97833b
Krushil Ponkiya <64345047+KrushilPonkiya@users.noreply.github.com>
2022-07-28 19:12:53 +0530

    change name

--- checkout/settings.py
+++ checkout/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 from pathlib import Path
 import os
-import django_heroku
+# import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,7 +127,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/Media/'
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
