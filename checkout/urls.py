4c9b59cb6612cf1a615561ef5946a00e1b5d2618
Krushil Ponkiya <64345047+KrushilPonkiya@users.noreply.github.com>
2022-07-28 17:29:39 +0530

    add requirements

--- checkout/urls.py
+++ checkout/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('api/', include('api.urls')),
+    
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
