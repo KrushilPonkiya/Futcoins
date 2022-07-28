2869efbd7c1dab15f45cb4f9ada1003dc7553dff
Krushil Ponkiya <64345047+KrushilPonkiya@users.noreply.github.com>
2022-07-28 12:02:14 +0530

    add package

--- checkout/urls.py
+++ checkout/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('api/', include('api.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
++ static(settings.STATIC_URL, document_root=settings.STATIC_STATICFILES_DIRS)
